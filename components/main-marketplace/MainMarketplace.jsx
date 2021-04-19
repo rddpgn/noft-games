39bd9dc41eb5052557a209a7b2dfbdb2145d80c0
rddpgn <rddpgn@gmail.com>
2021-04-20 01:54:51 +0300

    Cards adaptive

--- components/main-marketplace/MainMarketplace.jsx
+++ components/main-marketplace/MainMarketplace.jsx
@@ -13,57 +13,42 @@ const settings = {
     slidesToScroll: 1
 };
 
-const getWindowDimensions = () => {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-        width,
-        height
-    };
-}
-
-const useWindowDimensions = () => {
-    if (typeof window === 'undefined') return { width: 0, height: 0};
-    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-    useEffect(() => {
-        function handleResize() {
-            setWindowDimensions(getWindowDimensions());
-        }
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    return windowDimensions;
-}
-
 const MainMarketplace = () => {
     const [cardsState, setOpenedCardId] = useState([false, false, false, false, false, false]);
     const onClickCallback = (cardId, state) => setOpenedCardId(cardsState.map((cardState, index) => index === cardId && state));
-    const { height, width } = useWindowDimensions();
 
     return (
         <section>
             <h2>MARKETPLACE</h2>
-            {width >= 600 ? (
-                <div className={styles.marketplace_container}>
-                    <HeroCard opened={cardsState[0]} onclick={onClickCallback} cardId={0}/>
-                    <HeroCard opened={cardsState[1]} onclick={onClickCallback} cardId={1}/>
-                    <HeroCard opened={cardsState[2]} onclick={onClickCallback} cardId={2}/>
-                    <HeroCard opened={cardsState[3]} onclick={onClickCallback} cardId={3}/>
-                    <HeroCard opened={cardsState[4]} onclick={onClickCallback} cardId={4}/>
-                    <HeroCard opened={cardsState[5]} onclick={onClickCallback} cardId={5}/>
-                </div>
-            ):(
-                <Slider {...settings}>
-                    <HeroCard opened={cardsState[0]} onclick={onClickCallback} cardId={0}/>
-                    <HeroCard opened={cardsState[1]} onclick={onClickCallback} cardId={1}/>
-                    <HeroCard opened={cardsState[2]} onclick={onClickCallback} cardId={2}/>
-                    <HeroCard opened={cardsState[3]} onclick={onClickCallback} cardId={3}/>
-                    <HeroCard opened={cardsState[4]} onclick={onClickCallback} cardId={4}/>
-                    <HeroCard opened={cardsState[5]} onclick={onClickCallback} cardId={5}/>
-                </Slider>
-            )}
+            <div className={styles.marketplace_container}>
+                <HeroCard opened={cardsState[0]} onclick={onClickCallback} cardId={0}/>
+                <HeroCard opened={cardsState[1]} onclick={onClickCallback} cardId={1}/>
+                <HeroCard opened={cardsState[2]} onclick={onClickCallback} cardId={2}/>
+                <HeroCard opened={cardsState[3]} onclick={onClickCallback} cardId={3}/>
+                <HeroCard opened={cardsState[4]} onclick={onClickCallback} cardId={4}/>
+                <HeroCard opened={cardsState[5]} onclick={onClickCallback} cardId={5}/>
+            </div>
+            <Slider {...settings} className={styles.slider}>
+                <HeroCard opened={cardsState[0]} onclick={onClickCallback} cardId={0}/>
+                <HeroCard opened={cardsState[1]} onclick={onClickCallback} cardId={1}/>
+                <HeroCard opened={cardsState[2]} onclick={onClickCallback} cardId={2}/>
+                <HeroCard opened={cardsState[3]} onclick={onClickCallback} cardId={3}/>
+                <HeroCard opened={cardsState[4]} onclick={onClickCallback} cardId={4}/>
+                <HeroCard opened={cardsState[5]} onclick={onClickCallback} cardId={5}/>
+            </Slider>
+        </section>
+    )
+    return (
+        <section>
+            <h2>MARKETPLACE</h2>
+            <Slider {...settings}>
+                <HeroCard opened={cardsState[0]} onclick={onClickCallback} cardId={0}/>
+                <HeroCard opened={cardsState[1]} onclick={onClickCallback} cardId={1}/>
+                <HeroCard opened={cardsState[2]} onclick={onClickCallback} cardId={2}/>
+                <HeroCard opened={cardsState[3]} onclick={onClickCallback} cardId={3}/>
+                <HeroCard opened={cardsState[4]} onclick={onClickCallback} cardId={4}/>
+                <HeroCard opened={cardsState[5]} onclick={onClickCallback} cardId={5}/>
+            </Slider>
         </section>
     )
 };
